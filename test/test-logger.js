a1d960f44759c7ccb7dd17a458660d2844299afd
Cristian Cavalli <cristiancavall@google.com>
2016-08-16 01:18:27 -0700

    Increase test coverage >95%
    Increase test coverage to cover current API. Add logic
    to `logger.js` to respect given log limits. Add npm
    coverage script and add configuration in .travis.yml to
    export the stub credentials file used for testing client
    configuration. Add coverage to `.gitignore` to ignore
    generated files from added npm coverage script.
    
    Dependencies Added:
    `mock-fs` - Added to mock logging so that log statements can be
      analyzed for formatting correctness.
    
    08/16/2016 22:50PST
    Respond to pr feedback by removing forgotten `console.log` call.
    Node 6.4.0 is introducing some unexpected output for newlines in
    util.inspect by producing \r\n. This has been normalized in the
    test since it is a newline, but this is the only function exhibiting
    this behavior as of now and it seems amiss. Test was updated
    accordingly with documentation.
    
    08/18/2016 14:00PST
    Respond to pr feedback - adjust authorizedRequestFactory tests
    to only use a `.once` instance of nock for single request tests.

--- test/test-logger.js
+++ test/test-logger.js
@@ -15,9 +15,15 @@
  */
 
 'use strict';
-
+var os = require('os');
+var fs = require('fs');
+var fsMock = require('mock-fs');
+var proxyquire = require('proxyquire').noPreserveCache();
 var assert = require('assert');
-var logger = require('../lib/logger.js');
+var PREFIX = 'mock_fd_logger';
+var moment = require('moment');
+var MOCK_FILE = [PREFIX, 'log.txt'].join('_');
+var MOCK_PATH = os.tmpdir()+'/'+MOCK_FILE; // mock-fs always uses forward slashes
 
 /* Stub console.log for testing */
 var buffer = [];
@@ -27,8 +33,223 @@ console._stdout.write = function() {
   orig.apply(this, arguments);
 };
 
+describe('mocking fd logger with config', function () {
+  it('should write an entry to the fs mock when log is called', function () {
+    var logger = require('../lib/logger.js');
+    var l = logger.create(logger.SILLY, PREFIX, 9);
+    for (var i = 0; i < 15; i += 1) {
+      l.info('info_');
+    }
+    var s = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual(s.split('\n').length, 10, 'There should be 10 entries in the logs')
+  });
+  before(function () {
+    var mockTree = {};
+    mockTree[os.tmpdir()] = {};
+    mockTree[os.tmpdir()][MOCK_FILE] = '';
+    fsMock(mockTree, {createTmp: false});
+  });
+  after(function () {
+    fsMock.restore();
+  });
+});
 
-describe('logger', function() {
+describe('mocking fd logger without config', function () {
+  it('should not write an entry to the fs mock when log is called', function () {
+    var logger = require('../lib/logger.js');
+    var l = logger.create();
+    for (var i = 0; i < 5; i += 1) {
+      l.info('info_');
+    }
+    var s = fs.readFileSync(os.tmpdir()+'/_log.txt').toString();
+    assert.deepEqual(s, '', 'The log file should be empty');
+  });
+  before(function () {
+    var mockTree = {};
+    mockTree[os.tmpdir()] = {};
+    mockTree[os.tmpdir()]['_log.txt'] = '';
+    fsMock(mockTree, {createTmp: false});
+  });
+  after(function () {
+    fsMock.restore();
+  });
+});
+
+describe('logging a breakpoint object', function () {
+  var checkTime = /createdTime/;
+  var checkCondition = /condition/;
+  var checkExpression = /expressions/;
+  it('should log a breakpoint object correctly', function () {
+    var logger = require('../lib/logger.js');
+    var correctOutput = [
+      'ERROR:mock_fd_logger: breakpoint id: 0,\n\tlocation: { line: 3, path:',
+      ' \'/my/project/root/test/fixtures/a/hello.js\' }\n\tcreatedTime: ',
+      moment.unix(parseInt(1471332053, 10)).calendar(),
+      '\n\tcondition: \'if n == 3 then true else false\'\n\texpressions: ',
+      '[ \'if n == 3 then Math.PI * n else n\' ]\n'
+    ].join('');
+    var bp = {
+      id: 0,
+      location: {
+        line: 3,
+        path: '/my/project/root/test/fixtures/a/hello.js'
+      },
+      createdTime: {seconds: 1471332053},
+      condition: 'if n == 3 then true else false',
+      expressions: ['if n == 3 then Math.PI * n else n']
+    };
+    var l = logger.create(logger.SILLY, PREFIX, 10);
+    l.breakpoint(logger.ERROR, '', bp);
+    var s = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual(s, correctOutput, 'The log break point should be properly formatted');
+  });
+  it('should log a breakpoint without time correctly', function () {
+    var log, writer;
+    var logger = require('../lib/logger.js');
+    var bp = {
+      id: 0,
+      location: {
+        line: 3,
+        path: '/my/project/root/test/fixtures/a/hello.js'
+      },
+      condition: 'if n == 3 then true else false',
+      expressions: ['if n == 3 then Math.PI * n else n']
+    };
+    writer = logger.create(logger.SILLY, PREFIX, 10);
+    writer.breakpoint(logger.ERROR, '', bp);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.ok(!checkTime.test(log));
+    assert.ok(checkCondition.test(log));
+    assert.ok(checkExpression.test(log));
+  });
+  it('should log a breakpoint without condition correctly', function () {
+    var log, writer;
+    var logger = require('../lib/logger.js');
+    var bp = {
+      id: 0,
+      location: {
+        line: 3,
+        path: '/my/project/root/test/fixtures/a/hello.js'
+      },
+      createdTime: {seconds: 1471332053},
+      expressions: ['if n == 3 then Math.PI * n else n']
+    };
+    writer = logger.create(logger.SILLY, PREFIX, 10);
+    writer.breakpoint(logger.ERROR, '', bp);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.ok(checkTime.test(log));
+    assert.ok(!checkCondition.test(log));
+    assert.ok(checkExpression.test(log));
+  });
+  it('should log a breakpoint without expressions correctly', function () {
+    var log, writer;
+    var logger = require('../lib/logger.js');
+    var bp = {
+      id: 0,
+      location: {
+        line: 3,
+        path: '/my/project/root/test/fixtures/a/hello.js'
+      },
+      createdTime: {seconds: 1471332053},
+      condition: 'if n == 3 then true else false'
+    };
+    writer = logger.create(logger.SILLY, PREFIX, 10);
+    writer.breakpoint(logger.ERROR, '', bp);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.ok(checkTime.test(log));
+    assert.ok(checkCondition.test(log));
+    assert.ok(!checkExpression.test(log));
+  });
+  it('should not log a breakpoint if the logging level is set lower', function () {
+    var log, writer;
+    var logger = require('../lib/logger.js');
+    var bp = {
+      id: 0,
+      location: {
+        line: 3,
+        path: '/my/project/root/test/fixtures/a/hello.js'
+      },
+      createdTime: {seconds: 1471332053},
+      condition: 'if n == 3 then true else false'
+    };
+    writer = logger.create(logger.ERROR, PREFIX, 10);
+    writer.breakpoint(logger.DEBUG, '', bp);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual('', log, 'The log should be empty');
+  });
+  it('should log an array of break points', function () {
+    var correctOutput = [
+      'ERROR:mock_fd_logger: \nERROR:mock_fd_logger: breakpoint id: 0,',
+      '\n\tlocation: { line: 3, path: \'/my/project/root/test/fixtures/',
+      'a/hello.js\' }\n\tcreatedTime: ',
+      moment.unix(parseInt(1471332053, 10)).calendar(),
+      '\n\tcondition: \'',
+      'if n == 3 then true else false\'\nERROR:mock_fd_logger: breakpoint ',
+      'id: 1,\n\tlocation: { line: 10,\n  path: ',
+      '\'/my/project/root/test/fixtures/a/goodbye.js\' }\n\tcreatedTime: ',
+      moment.unix(parseInt(1471332553, 10)).calendar(),
+      '\n\tcondition: \'if n == 4 then true else false\'\n'
+    ].join('');
+    var log, writer;
+    var logger = require('../lib/logger.js');
+    var bps = [
+      {
+        id: 0,
+        location: {
+          line: 3,
+          path: '/my/project/root/test/fixtures/a/hello.js'
+        },
+        createdTime: {seconds: 1471332053},
+        condition: 'if n == 3 then true else false'
+      },
+      {
+        id: 1,
+        location: {
+          line: 10,
+          path: '/my/project/root/test/fixtures/a/goodbye.js'
+        },
+        createdTime: {seconds: 1471332553},
+        condition: 'if n == 4 then true else false'
+      }
+    ];
+    writer = logger.create(logger.DEBUG, PREFIX, 10);
+    writer.breakpoints(logger.SILLY, '', bps);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual('', log, 'The log should be empty');
+    writer.breakpoints(logger.ERROR, '', bps);
+    // cristiancavalli added the \r\n replace with \n since it looks
+    // like utils.inspect is changing behavior in node v6.4.0 and
+    // newlining with \r\n instead of just \n
+    // @TODO figure out if this is intended and why - the rest of the
+    // ouput uses /n and other versions pass without this
+    log = fs.readFileSync(MOCK_PATH).toString().replace(/\r\n/g, '\n');
+    assert.deepEqual(correctOutput, log, 'The log should be properly formatted');
+  });
+  it('should log an interval', function () {
+    var log, writer;
+    var correctOutput = 'ERROR:mock_fd_logger: test 100000.0002ms\n';
+    var logger = require('../lib/logger.js');
+    writer = logger.create(logger.ERROR, PREFIX, 10);
+    writer.interval(logger.DEBUG, 'test', 1000);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual('', log, 'The log should be empty');
+    writer.interval(logger.ERROR, 'test', [100, 200]);
+    log = fs.readFileSync(MOCK_PATH).toString();
+    assert.deepEqual(correctOutput, log, 'The log should be properly formatted');
+  });
+  beforeEach(function () {
+    var mockTree = {};
+    mockTree[os.tmpdir()] = {};
+    mockTree[os.tmpdir()][MOCK_FILE] = '';
+    fsMock(mockTree, {createTmp: false});
+  });
+  afterEach(function () {
+    fsMock.restore();
+  });
+});
+
+describe('logger base-functionality', function() {
+  var logger = require('../lib/logger.js');
   it('should return a logger through the create function', function() {
     var l = logger.create(logger.DEBUG, __filename);
     assert.ok(l);
@@ -69,7 +290,4 @@ describe('logger', function() {
     l.debug('d');
     assert.ok(buffer.length === 0);
   });
-
-  it('should correctly log a breakpoint object');
-  it('should correctly log an array of breakpoints');
 });
